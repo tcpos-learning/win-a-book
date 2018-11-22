68f44867fe91554112ee1bc5c582ca77abcf09f7
Martini Arialdo <arialdo.martini@tcpos.com>
2018-11-22 09:46:52 +0100

    Tests for negative numbers

--- MaxWithoutCompare.Test/MaxCalculatorTest.cs
+++ MaxWithoutCompare.Test/MaxCalculatorTest.cs
@@ -5,18 +5,35 @@ namespace MaxWithoutCompare.Test
 {
     public class MaxCalculatorTest
     {
+        private readonly Calculator _sut;
+
+        public MaxCalculatorTest()
+        {
+            _sut = new Calculator();            
+        }
+        
         [Theory]
         [InlineData(127, 212, 212)]
         [InlineData(25, 11, 25)]
+        [InlineData(100, 100, 100)]
+        [InlineData(0, 100, 100)]
+        [InlineData(100, 0, 100)]
+        public void should_identify_the_biggest_between_two_numbers(int first, int second, int expected)
+        {
+            var result = _sut.Max(first, second);
+
+            result.Should().Be(expected);
+        }
+        
+        [Theory]
         [InlineData(1, -2, 1)]
         [InlineData(-10, -2, -2)]
-        [InlineData(100, 100, 100)]
         [InlineData(-15, 19, 19)]
-        public void should_(int first, int second, int expected)
+        [InlineData(-15, 0, 0)]
+        [InlineData(0, -15, 0)]
+        public void should_identity_the_biggest_between_two_numbers_also_with_negative_numbers(int first, int second, int expected)
         {
-            var sut = new Calculator();
-
-            var result = sut.Max(first, second);
+            var result = _sut.Max(first, second);
 
             result.Should().Be(expected);
         }
